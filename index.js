31b406c1832aac3c5ecd1c9ada13ec60ffd6c5ed
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-04-02 05:11:21 +0600

    add some code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
 const app = express()
 const port = process.env.PORT || 5000
@@ -6,13 +8,20 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.use(cors());
+app.use(bodyParser.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ar51s.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("test").collection("devices");
   
-  console.log('database connect successfully')
+  app.post('/products', (req, res) => {
+    const product = req.body;
+    productsCollection.insertOne(product)
+    .then(result => {
+        console.log(result)
+    })
+  })
 });
 
 
