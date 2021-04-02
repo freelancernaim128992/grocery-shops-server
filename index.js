728595aa7a3107416056d99bb6cc6494ec6044e0
Arafat Islam Naim <freelancernaim128992@gmail.com>
2021-04-02 18:19:19 +0600

    add some code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -15,6 +16,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db(`${process.env.DB_NAME}`).collection('products');
+  const ordersCollection = client.db(`${process.env.DB_NAME}`).collection('orders');
   app.post('/products', (req, res) => {
     const product = req.body;
     productsCollection.insertOne(product)
@@ -26,6 +28,35 @@ client.connect(err => {
     productsCollection.find({})
     .toArray((err, document) => {
       res.send(document);
+    })
+  })
+  app.get('/product/:id', (req, res) => {
+    const singleProduct = ObjectId(req.params.id);
+    productsCollection.find({_id: singleProduct})
+    .toArray((err, document) => {
+      if(document[0]){
+        const newDocument = {...document[0]}
+        newDocument.quantity = 1
+        res.send(newDocument)
+        console.log(newDocument)
+      }
+    })
+
+  })
+
+  app.post('/orders',(req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+    .then(result => {
+      console.log(result)
+      console.log('data loaded successfully');
+    })
+  })
+
+  app.get('/orderList', (req,res) => {
+    ordersCollection.find({})
+    .toArray((err, document) => {
+      res.send(document)
     })
   })
 });
